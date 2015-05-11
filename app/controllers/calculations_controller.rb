9d4f5ce9f7029ae2cc04cba003ab59d99e8ecbda
atrivedi22 <atrivedi2016@kellogg.northwestern.edu>
2015-05-10 21:23:53 -0500

    practice1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,9 +2,21 @@ class CalculationsController < ApplicationController
   def instructions
   end
 
+def square
+  number=params["number"].to_i
+  @result= number*number
+end
+
   def sqrt
+    number=params["number"].to_i
+    @result=Math.sqrt(number)
   end
 
-  def pmt
+ def pmt
+interest_rate=params["interest_rate"].to_i
+payments=params["payments"].to_i
+principal=params["principal"].to_i
+@result= principal*interest_rate/(1-1/(1+interest_rate)**payments)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+get("/square_root/:number/", { :controller => "calculations", :action => "sqrt" })
+
+get("/square/:number/", { :controller => "calculations", :action => "square" })
+
+get("/payment/:interest_rate/:payments/:principal", {:controller => "calculations", :action=>"pmt"})
+
   get("/", { :controller => "calculations", :action => "instructions" })
 end
+
+
+
+
+ #http://localhost:3000/payment/34/60/30000
+#<code>/payment/INTEREST_RATE/NUMBER_OF_PAYMENTS/PRINCIPAL_VALUE</code>
